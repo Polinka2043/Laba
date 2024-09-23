52be6cb23eff6ef94693799ae4d4c2f9c7e78eb8
vepre_pp <vepre.pp.03@mail.ru>
2024-09-23 22:23:16 +0300

    sdfhks

--- add.py
+++ add.py
@@ -1,21 +1,37 @@
+from flask import Flask, request, render_template_string
 import requests
-from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 def index():
-    if request.method == 'POST':
-        text = request.form['text']
-        # Send the text to the backend
-        response = requests.post('http://localhost:5001/receive', data={'text': text})
-        return 'Text sent to backend!'
     return '''
-        <form action="" method="post">
-            <input type="text" name="text" placeholder="Enter some text">
-            <input type="submit" value="Отправить">
-        </form>
+        <html>
+            <head>
+                <title>Frontend 2.0</title>
+            </head>
+            <body>
+                <h1>Frontend 2.0</h1>
+                <form action="/api/data" method="post">
+                    <input type="text" name="userInput" placeholder="Enter some text">
+                    <input type="submit" value="Send to Backend">
+                </form>
+                <div id="result-field">{{ result }}</div>
+            </body>
+        </html>
     '''
 
+@app.route('/api/data', methods=['POST'])
+def send_to_backend():
+    user_input = request.form['userInput']
+    response = requests.post('http://backend:5000/api/data', json={'userInput': user_input})
+    return render_template_string('''
+        <html>
+            <body>
+                <div id="result-field">{{ response.text }}</div>
+            </body>
+        </html>
+    ''', response=response)
+
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True)
